7d17490fe80c69b396ee8393b5452ebd16e521eb
kyryloren <dev@kyryloorlov.com>
2025-05-25 04:17:09 -0400

    Fixed that

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 const nextConfig = {
-  experimental: {
-    optimizeCss: true,
-    urlImports: ['https://cdn.skypack.dev', 'https://unpkg.com'],
-    webVitalsAttribution: ['CLS', 'LCP'],
-  },
   compiler: {
     removeConsole: process.env.NODE_ENV !== 'development',
-    styledComponents: true,
   },
   images: {
     remotePatterns: [
@@ -15,6 +9,36 @@ const nextConfig = {
         hostname: process.env.STRAPI_HOSTNAME,
       },
     ],
+  },
+  headers: async () => {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN',
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          },
+        ],
+      },
+      {
+        source: '/_next/static/css/(.*)',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'text/css',
+          },
+        ],
+      },
+    ]
   },
   redirects: async () => {
     return [
