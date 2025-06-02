a2c28f6de941e04513a3b0c684fe516e3177c28e
kyryloren <dev@kyryloorlov.com>
2025-06-02 16:09:57 -0400

    Edited size

--- styles/text.js
+++ styles/text.js
@@ -25,5 +25,5 @@ export function SmallTextClass(className) {
   )
 }
 export function BaseTextClass(className) {
-  return twMerge('text-base leading-4', className)
+  return twMerge('2xl:text-(length:--base-desktop) 2xl:leading-(--base-leading-desktop) text-base leading-4', className)
 }
