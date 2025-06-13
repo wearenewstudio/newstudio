8491d85bd935a7eca5086e8e38bc11920d5d6036
kyryloren <dev@kyryloorlov.com>
2025-06-13 18:48:15 -0400

    Added line spacing

--- app/about/components/studio/index.js
+++ app/about/components/studio/index.js
@@ -1,10 +1,10 @@
-import { HugeTextClass } from 'styles'
+import { HugeTextClass, RenderText } from 'styles'
 import Wrapper from '../wrapper'
 
 export default function Studio({ data }) {
   return (
     <Wrapper title={'Our Studio'}>
-      <p className={HugeTextClass()}>{data}</p>
+      <RenderText text={data} className={HugeTextClass()} />
     </Wrapper>
   )
 }
